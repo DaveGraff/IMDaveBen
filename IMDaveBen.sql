4d58e36d3d40ead9e071b5876a1e3031ed2e2ccc
David Graff <33940576+DaveGraff@users.noreply.github.com>
2018-11-18 15:52:53 -0500

    Fixed Formatting

--- IMDaveBen.sql
+++ IMDaveBen.sql
@@ -1,37 +1,36 @@
 CREATE TABLE Person(
-		Id				INTEGER,
+	Id			INTEGER,
     	PersonName		CHAR(40) NOT NULL,
-    	DOB				CHAR(8), -- MM/DD/YYYY
+    	DOB			CHAR(8), -- MM/DD/YYYY
     	Hometown		CHAR(30),
     	Height			CHAR(10),
     	Gender			ENUM('Male', 'Female', 'Intersex'),
     	Biography		TEXT,
-		CHECK(LEN(DOB) = 8),
+	CHECK(LEN(DOB) = 8),
     	PRIMARY KEY(Id));
     
 CREATE TABLE Studio(
-		StudioName 		CHAR(30),
-		Location		CHAR(15),
+	StudioName 		CHAR(30),
+	Location		CHAR(15),
     	PRIMARY KEY(StudioName, Location));
 
 CREATE TABLE Shows(
-		ShowTitle		CHAR(30),
+	ShowTitle		CHAR(30),
     	ShowYear		INTEGER,
     	Genre			CHAR(40),
-		Reviews			DOUBLE(2,2), -- xx.xx/10
+	Reviews			DOUBLE(2,2), -- xx.xx/10
     	Runtime			INTEGER,
-    	Rating			ENUM('G', 'PG', 'PG-13', 'R', 'NC-17', 'NR', 'TV-Y', 'TV-Y7', 'TV-G', 'TV-PG', 
-'TV-14', 'TV-MA'),
+    	Rating			ENUM('G', 'PG', 'PG-13', 'R', 'NC-17', 'NR', 'TV-Y', 'TV-Y7', 'TV-G', 'TV-PG', 'TV-14', 'TV-MA'),
     	Synopsis		TEXT,
-		StudioName		CHAR(30),
+	StudioName		CHAR(30),
     	StudioLocation	CHAR(3) DEFAULT 'USA', -- 3 digit country code
-		IsTVShow		BOOLEAN,
-		CHECK(ShowYear > 1850 AND ShowYear < 2019),
+	IsTVShow		BOOLEAN,
+	CHECK(ShowYear > 1850 AND ShowYear < 2019),
     	FOREIGN KEY (StudioName, StudioLocation) REFERENCES Studio(StudioName, Location),
     	PRIMARY KEY(ShowTitle, ShowYear));
 
 CREATE TABLE CrewMembers(
-		PersonID		INTEGER,
+	PersonID		INTEGER,
     	Role			CHAR(30),
     	ShowTitle		CHAR(30),
     	ShowYear		INTEGER,
@@ -41,7 +40,7 @@ CREATE TABLE CrewMembers(
 
 
 CREATE TABLE CastMembers(
-		PersonID		INTEGER,
+	PersonID		INTEGER,
     	CharacterName		CHAR(30),
     	Role			CHAR(30),
     	ShowTitle		CHAR(30),
@@ -51,24 +50,24 @@ CREATE TABLE CastMembers(
     	FOREIGN KEY(PersonID) REFERENCES Person(Id));
 
 CREATE TABLE Award(
-		PersonID		INTEGER,
+	PersonID		INTEGER,
     	ShowTitle		CHAR(30),
     	ShowYear		INTEGER,
-    	Academy		CHAR(30),
-		AwardName		CHAR(30),
+    	Academy			CHAR(30),
+	AwardName		CHAR(30),
     	YearAwarded		INTEGER,    	
-		CHECK(YearAwarded > 1850 AND YearAwarded < 2019),
-		PRIMARY KEY(Academy, AwardName, YearAwarded),
+	CHECK(YearAwarded > 1850 AND YearAwarded < 2019),
+	PRIMARY KEY(Academy, AwardName, YearAwarded),
     	FOREIGN KEY(PersonID) REFERENCES Person(Id),
     	FOREIGN KEY(ShowTitle, ShowYear) REFERENCES Shows(ShowTitle, ShowYear));
     
 CREATE TABLE Episode(
-		EpisodeName	CHAR(30) NOT NULL,
-    	ShowTitle	CHAR(30),
-    	ShowYear	CHAR(30),
-		EpisodeNum	INTEGER,
-    	Season		INTEGER,
-    	Synopsis	TEXT,
+	EpisodeName		CHAR(30) NOT NULL,
+    	ShowTitle		CHAR(30),
+    	ShowYear		CHAR(30),
+	EpisodeNum		INTEGER,
+    	Season			INTEGER,
+    	Synopsis		TEXT,
     	CHECK(EpisodeNum > 0 AND Season > 0),
     	PRIMARY KEY(ShowTitle, ShowYear, EpisodeNum, Season),
     	FOREIGN KEY(ShowTitle, ShowYear) REFERENCES Shows(ShowTitle, ShowYear));
