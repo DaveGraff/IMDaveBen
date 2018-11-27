71f5ecb389e614a67a8e97c8e0bfd0eca559d18e
Benjamin Shu <benjamin.shu@stonybrook.edu>
2018-11-26 21:01:24 -0500

    Implemented request handling for CSS and logo image.

--- IMDaveBen.js
+++ IMDaveBen.js
@@ -6,9 +6,18 @@ var dir = path.dirname(require.main.filename);
 const express = require('express');
 const app = new express();
 
-app.get('/', function(request, response){
-	response.sendFile(dir + '\\CSE305Search.html');
+app.get('/', function(req, res){
+	res.sendFile(dir + '\\CSE305Search.html');
 });
+
+app.get('/style.css', function(req, res){
+	res.sendFile(dir + '\\style.css');
+});
+
+app.get('/logo.png', function(req, res){
+	res.sendFile(dir + '\\logo.png');
+});
+
 app.listen(8080);
 
 var con = mysql.createConnection({
