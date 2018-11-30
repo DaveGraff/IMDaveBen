68745fcf334f34eda9a6d64a1b8829a9052f615e
Benjamin Shu <benjamin.shu@stonybrook.edu>
2018-11-30 03:38:42 -0500

    Implemented separate HTML page for responding to server queries. Had a long struggle with asynchronous functions in Node.js.

--- IMDaveBen.js
+++ IMDaveBen.js
@@ -13,6 +13,10 @@ app.get('/', function(req, res){
 	res.sendFile(dir + '\\CSE305Search.html');
 });
 
+app.get('/CSE305Response.html', function (req, res) {
+	res.sendFile(dir + '\\CSE305Response.html');
+});
+
 app.get('/style.css', function(req, res){
 	res.sendFile(dir + '\\style.css');
 });
@@ -23,35 +27,59 @@ app.get('/logo.png', function(req, res){
 
 app.post('/CSE305Response.html', (req, res) => {
 	const postBody = req.body;
+	var page;
 	console.log(postBody);
 	if (postBody.type == 'People') {
 		console.log("People");
 		getPeople(postBody.name, function (person) {
 			if (person == undefined || person.length == 0) {
 				console.log("No results found!");
-				buildPage("Null", person);
+				page = buildPage("Null", person);
+				page.then(
+				function(result) {
+					if (result) { console.log('Finished writing response!'); }
+					res.sendFile(dir + '\\CSE305Response.html');
+				},
+				function(err) { console.log(err); });
 			} else {
 				console.log(person);
-				buildPage(postBody.type, person);
+				page = buildPage(postBody.type, person, res);
+				page.then(
+				function(result) {
+					if (result) { console.log('Finished writing response!'); }
+					res.sendFile(dir + '\\CSE305Response.html');
+				},
+				function(err) { console.log(err); });
 			}
-			res.sendFile(dir + "\\CSE305Response.html");
 		});
 	} else if (postBody.type == 'Titles') {
 		console.log("Titles");
 		getTitles(postBody.name, function (title) {
 			if (title == undefined || title.length == 0) {
 				console.log("No results found!");
-				buildPage("Null", title);
+				page = buildPage(postBody.type, title, res);
+				page.then(
+				function(result) {
+					if (result) { console.log('Finished writing response!'); }
+					res.sendFile(dir + '\\CSE305Response.html');
+				},
+				function(err) { console.log(err); });
 			} else {
 				console.log(title);
-				buildPage(postBody.type, title);
+				page = buildPage(postBody.type, title, res);
+				page.then(
+				function(result) {
+					if (result) { console.log('Finished writing response!'); }
+					res.sendFile(dir + '\\CSE305Response.html');
+				},
+				function(err) { console.log(err); });
 			}
-			res.sendFile(dir + "\\CSE305Response.html");
 		});
 	}
 });
 
-app.listen(8080);
+const server = app.listen(8080);
+server.keepAliveTimeout = 60000 * 2;
 
 var con = mysql.createConnection({
 	database : 'IMDaveBen',
@@ -101,25 +129,26 @@ function getTitle(name, year, callback){
 var fs = require('fs');
 var filename = dir + '\\CSE305Response.html';
 
-function buildPage(type, res) {
-	fs.truncate(filename, 0, function() {console.log("Response file cleared!");});
-	var stream = fs.createWriteStream(filename);
-	stream.once('open', function(fd) {
-		var html = buildHTML(type, res);
-		stream.end(html);
+function buildPage(type, list, res) {
+	return new Promise((resolve, reject) => {
+		fs.truncate(filename, 0, function() {console.log("Response file cleared!");});
+		var body = '';
+		if ((type == 'Titles' || type == 'People') && list != '') {
+			console.log('Appending results...');
+			body += list;
+		} else {
+			body += "<h1>No results found!</h1>";
+		}
+		var html = ('<!DOCTYPE html><html><head><link rel=\"stylesheet\" href=\"style.css\"></head><body><img src=\"logo.png\" alt=\"IMDaveBen\" class = \"centerImage\">'
+					+ '<div class=\"center\">' + body + '<a href=\"/\">Back to query page</a></div></body></html>');
+		fs.writeFile(dir + "\\CSE305Response.html", html, function (err) {
+			if (err) { reject(err);	}
+			resolve(true);
+		});
 	});
 }
 
 //Generate HTML code for response
-function buildHTML(type, res) {
-	var body = '';
-	if (type == 'Titles' || type == 'People') {
-		body += res;
-	} else {
-		body += "<h1 class=\"center\">No results found!</h1>";
-	}
-	return '<!DOCTYPE html>' + '<html><head>'
-	+ '<link rel=\"stylesheet\" href=\"style.css\"><title>IMDaveBen</title></head>'
-	+ '<body>'	+ body + '</body></html>';
+function buildHTML(type, list) {
+	
 }
-
